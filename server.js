7681d247435eae6c22e8af33ba284a4509e95ca1
janae-b <janae.boersma@comcast.net>
2021-03-23 20:36:18 -0500

    updated styling

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('newerror');
+  res.render('error');
 });
 
 module.exports = app;
